953a4604ea22d2bc3a43bf2fe91140a1a952b94a
Sander Sitkin <69676803+sandersitkin@users.noreply.github.com>
2025-05-07 15:27:37 +0300

    Add theme picker

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,18 @@
-/**
- * Toggles the mobile menu.
- */
-function toggleMobileMenu() {
+// Toggle mobile menu
+const menuButton = document.querySelector(".menu-button");
+
+menuButton.addEventListener("click", () => {
 	document.body.classList.toggle("show-menu");
-}
+});
+
+
+// Theme picker
+const themePicker = document.querySelector(".theme-picker");
+
+themePicker.value = theme;
+
+themePicker.addEventListener("change", event => {
+	theme = event.target.value;
+	document.documentElement.dataset.theme = theme;
+	localStorage.setItem("theme", theme);
+});
